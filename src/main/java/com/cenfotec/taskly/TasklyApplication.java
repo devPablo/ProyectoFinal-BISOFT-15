411ea7375875b44d9c50612a10e79d06275b1040
Pablo Bonilla <pbonilla29@hotmail.com>
2020-04-23 14:43:30 -0600

    Update app initialization

--- src/main/java/com/cenfotec/taskly/TasklyApplication.java
+++ src/main/java/com/cenfotec/taskly/TasklyApplication.java
@@ -2,9 +2,16 @@ package com.cenfotec.taskly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TasklyApplication {
+public class TasklyApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TasklyApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TasklyApplication.class, args);

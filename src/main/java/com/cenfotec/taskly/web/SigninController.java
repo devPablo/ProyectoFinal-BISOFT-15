c10f29694217ab5f33021b88d933fa486aa11044
krausekelssen <kperezc@ucenfotec.ac.cr>
2020-04-30 14:58:02 -0600

    Fixes

--- src/main/java/com/cenfotec/taskly/web/SigninController.java
+++ src/main/java/com/cenfotec/taskly/web/SigninController.java
@@ -1,8 +1,12 @@
 package com.cenfotec.taskly.web;
 
+import com.cenfotec.taskly.domain.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class SigninController {
@@ -10,5 +14,11 @@ public class SigninController {
     @GetMapping({"/signin"})
     public String signin(Model model) {
         return "signin";
+    }
+
+    @RequestMapping(value = "/processForm", method= RequestMethod.POST)
+    public String processForm(@ModelAttribute(value="user") User user) {
+        System.out.println("Hola mundo");
+        return "index";
     }
 }
--- src/main/resources/static/js/index.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("signin").addEventListener("click", redirect);
-
-function redirect(){
-    window.location.href += "signin";
-}

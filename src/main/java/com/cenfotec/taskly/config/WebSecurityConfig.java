c64d3df1b0c87a9166754270a001b1ca1dabb321
krausekelssen <kperezc@ucenfotec.ac.cr>
2020-05-01 21:59:40 -0600

    update bug in autentications

--- src/main/java/com/cenfotec/taskly/config/WebSecurityConfig.java
+++ src/main/java/com/cenfotec/taskly/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,18 +17,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     //Necesario para evitar que la seguridad se aplique a los resources
     //Como los css, imagenes y javascripts
     String[] resources = new String[]{
-            "/include/*","/css/","/icons/","/img/","/js/","/layer/*"
+            "/include/*","/css/*","/icons/","/img/","/js/*","/layer/*"
     };
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
                 //El metodo authorizeRequests() permite restringir y/o dar acceso request HTTP
                 .authorizeRequests()
                 //antMatchers(): Lista de URL que corresponden a un RequestMapping como lo hacemos en los controladores.
                 .antMatchers(resources).permitAll() // permitAll(): Especifica que estas URLs son accesibles por cualquiera.
-                .antMatchers("/signin", "/api*").permitAll()
+                .antMatchers("/login*","/", "/signin*", "/api*", "/resources/**").permitAll()
                 //access(): permite el acceso cumpliendo la expresión, en este caso tenemos la expresion “hasRole()”.
                 // Donde verifica si el usuario tiene ese especifico Role.
                 .antMatchers("/tasks*").access("hasRole('USER')")
@@ -54,6 +54,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
                 .permitAll()
                 //logoutSuccessUrl(): Indica la URL donde sera redirigido cuando el usuario cierre sesión.
                 .logoutSuccessUrl("/login?logout");
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws  Exception{
+        web.ignoring().antMatchers("/resources/**");
     }
 
     BCryptPasswordEncoder bCryptPasswordEncoder;
---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,4 @@
+document.querySelector("#redirectsignin").addEventListener("click",
+    function redireccionSignin() {
+        window.location.href = "http://localhost:8082/signin";
+    });
